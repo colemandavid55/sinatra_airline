7b9a9cf77b72c6ce4ed6704f75e7a598cd71efdf
David Coleman <colemandavid55@gmail.com>
2014-12-03 17:35:38 -0600

    Add functionality and routes to answer all questions in index

--- lib/ruby_db.rb
+++ lib/ruby_db.rb
@@ -12,6 +12,62 @@ class Airline
 
   end
 
+  def delayed_arrival
+    @db = PG.connect(host: 'localhost', dbname: 'airline_data')
+    sql = %q[
+            SELECT carrier, count(*)
+            FROM flights
+            WHERE arr_delay_new > 0
+            GROUP BY carrier
+            ORDER BY count desc
+            ]
+    result = @db.exec(sql).entries
+  end
+
+  def delayed_departure
+    @db = PG.connect(host: 'localhost', dbname: 'airline_data')
+    sql = %q[
+          SELECT origin_city_name, count(*)
+          FROM flights
+          WHERE dep_delay_new > 0 OR arr_delay_new > 0
+          GROUP by origin_city_name
+          ORDER BY count desc
+          ]
+    result = @db.exec(sql).entries
+  end
+
+  def arriving_late
+    @db = PG.connect(host: 'localhost', dbname: 'airline_data')
+    sql = %q[
+          SELECT dest_city_name, count(*)
+          FROM flights
+          WHERE dep_delay_new > 0 OR arr_delay_new > 0
+          GROUP by dest_city_name
+          ORDER by count desc
+          ] 
+    result = @db.exec(sql).entries
+  end
+
+  def average_late
+    @db = PG.connect(host: 'localhost', dbname: 'airline_data')
+    sql = %q[
+          SELECT avg(dep_delay_new)
+          FROM flights
+          ]
+    result = @db.exec(sql).entries
+  end
+
+  def average_airline
+    @db = PG.connect(host: 'localhost', dbname: 'airline_data')
+    sql = %q[
+          SELECT carrier, avg(dep_delay_new)
+          FROM flights
+          GROUP BY carrier
+          ORDER BY avg desc;
+          ]
+    result = @db.exec(sql).entries
+  end
+
 end
 
 
--- server.rb
+++ server.rb
@@ -14,3 +14,33 @@ get '/number_of_airlines' do
   @result = blah.airline_count
   erb :number_of_airlines
 end
+
+get '/delayed_arrival' do
+  blah = Airline.new
+  @result = blah.delayed_arrival
+  erb :delayed_arrival
+end
+
+get '/delayed_departure' do
+  blah = Airline.new
+  @result = blah.delayed_departure
+  erb :delayed_departure
+end
+
+get '/arriving_late' do
+  blah = Airline.new
+  @result = blah.arriving_late
+  erb :arriving_late
+end
+
+get '/average_late' do
+  blah = Airline.new
+  @result = blah.average_late
+  erb :average_late
+end
+
+get '/average_airline' do
+  blah = Airline.new
+  @result = blah.average_airline
+  erb :average_airline
+end
